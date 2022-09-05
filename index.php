fd35a17b30d4b33b405b76799dbc99fed6aebf7b
gfftr <gff.terl@gmail.com>
2022-09-05 13:51:39 +0100

    Added Datatable plugin

--- index.php
+++ index.php
@@ -1,112 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
- <!-- Required meta tags -->
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
-
- <!-- Bootstrap CSS -->
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title> PHP CRUD with Bootstrap Modal </title>
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
-
-
- <title>Hello, world!</title>
 </head>
 
 <body>
-
- <div class="px-4 py-5 my-5 text-center">
-  <h1 class="display-5 fw-bold">Update modal</h1>
-  <div class="col-lg-6 mx-auto">
-
-
-   <div class="card">
-    <h2> PHP CRUD Bootstrap MODAL (POP UP Modal) </h2>
-   </div>
-   <div class="card">
-    <div class="card-body">
-     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal">
-      ADD DATA
-     </button>
-    </div>
-   </div>
-
-
-
-   <div class="card">
-    <div class="card-body">
-
-
-     <?php
-     $connection = mysqli_connect("localhost", "root", "");
-     $db = mysqli_select_db($connection, 'students');
-     $query = "SELECT * FROM student";
-     $query_run = mysqli_query($connection, $query);
-     ?>
-
-
-     <table id="datatableid" class="table table-hover table-dark">
-      <thead>
-       <tr>
-        <th scope="col"> ID</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name </th>
-        <th scope="col"> Course </th>
-        <th scope="col"> Contact </th>
-        <th scope="col"> VIEW </th>
-        <th scope="col"> EDIT </th>
-        <th scope="col"> DELETE </th>
-       </tr>
-      </thead>
-
-
-      <?php
-      if ($query_run) {
-       foreach ($query_run as $row) {
-      ?>
-
-      <tbody>
-       <tr>
-        <td><?php echo $row['id']; ?></td>
-        <td><?php echo $row['fname']; ?> </td>
-        <td><?php echo $row['lname']; ?></td>
-        <td><?php echo $row['course']; ?> </td>
-        <td><?php echo $row['contact']; ?> </td>
-        <td>
-         <button type="button" class="btn btn-info viewbtn"> VIEW </button>
-        </td>
-        <td>
-         <button type="button" class="btn btn-success editbtn"> EDIT </button>
-        </td>
-        <td>
-         <button type="button" class="btn btn-danger deletebtn"> DELETE </button>
-        </td>
-       </tr>
-      </tbody>
-
-      <?php
-       }
-      } else {
-       echo "No Record Found";
-      }
-      ?>
-
-     </table>
-    </div>
-   </div>
-
-
-  </div>
- </div>
-
-
-
-
-
-
-
-
 
  <!-- Modal -->
  <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
@@ -201,11 +105,6 @@
   </div>
  </div>
 
-
-
-
-
-
  <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
  <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
   aria-hidden="true">
@@ -237,9 +136,109 @@
  </div>
 
 
+ <!-- VIEW POP UP FORM (Bootstrap MODAL) -->
+ <div class="modal fade" id="viewmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+   <div class="modal-content">
+    <div class="modal-header">
+     <h5 class="modal-title" id="exampleModalLabel"> View Student Data </h5>
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+     </button>
+    </div>
+
+    <form action="deletecode.php" method="POST">
+
+     <div class="modal-body">
 
 
 
+      <!-- <p id="fname"> </p> -->
+
+     </div>
+     <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal"> CLOSE </button>
+      <!-- <button type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button> -->
+     </div>
+    </form>
+
+   </div>
+  </div>
+ </div>
+
+
+ <div class="container">
+  <div class="jumbotron">
+   <div class="card">
+    <h2> PHP CRUD Bootstrap MODAL (POP UP Modal) </h2>
+   </div>
+   <div class="card">
+    <div class="card-body">
+     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal">
+      ADD DATA
+     </button>
+    </div>
+   </div>
+
+   <div class="card">
+    <div class="card-body">
+
+     <?php
+     $connection = mysqli_connect("localhost", "root", "");
+     $db = mysqli_select_db($connection, 'students');
+
+     $query = "SELECT * FROM student";
+     $query_run = mysqli_query($connection, $query);
+     ?>
+     <table id="datatableid" class="table table-bordered table-dark">
+      <thead>
+       <tr>
+        <th scope="col"> ID</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name </th>
+        <th scope="col"> Course </th>
+        <th scope="col"> Contact </th>
+        <th scope="col"> VIEW </th>
+        <th scope="col"> EDIT </th>
+        <th scope="col"> DELETE </th>
+       </tr>
+      </thead>
+      <?php
+      if ($query_run) {
+       foreach ($query_run as $row) {
+      ?>
+      <tbody>
+       <tr>
+        <td> <?php echo $row['id']; ?> </td>
+        <td> <?php echo $row['fname']; ?> </td>
+        <td> <?php echo $row['lname']; ?> </td>
+        <td> <?php echo $row['course']; ?> </td>
+        <td> <?php echo $row['contact']; ?> </td>
+        <td>
+         <button type="button" class="btn btn-info viewbtn"> VIEW </button>
+        </td>
+        <td>
+         <button type="button" class="btn btn-success editbtn"> EDIT </button>
+        </td>
+        <td>
+         <button type="button" class="btn btn-danger deletebtn"> DELETE </button>
+        </td>
+       </tr>
+      </tbody>
+      <?php
+       }
+      } else {
+       echo "No Record Found";
+      }
+      ?>
+     </table>
+    </div>
+   </div>
+
+
+  </div>
+ </div>
 
 
 
@@ -247,12 +246,45 @@
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 
+ <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+ <script>
+ $(document).ready(function() {
+
+  $('.viewbtn').on('click', function() {
+   $('#viewmodal').modal('show');
+
+  });
+
+ });
+ </script>
 
 
  <script>
- // delete
- $().ready(function() {
+ $(document).ready(function() {
+
+  $('#datatableid').DataTable({
+   "pagingType": "full_numbers",
+   "lengthMenu": [
+    [10, 25, 50, -1],
+    [10, 25, 50, "All"]
+   ],
+   responsive: true,
+   language: {
+    search: "_INPUT_",
+    searchPlaceholder: "Search Your Data",
+   }
+  });
+
+ });
+ </script>
+
+ <script>
+ $(document).ready(function() {
+
   $('.deletebtn').on('click', function() {
+
    $('#deletemodal').modal('show');
 
    $tr = $(this).closest('tr');
@@ -261,21 +293,19 @@
     return $(this).text();
    }).get();
 
+   console.log(data);
+
    $('#delete_id').val(data[0]);
 
   });
- })
+ });
  </script>
 
-
-
-
-
-
-
  <script>
- $().ready(function() {
+ $(document).ready(function() {
+
   $('.editbtn').on('click', function() {
+
    $('#editmodal').modal('show');
 
    $tr = $(this).closest('tr');
@@ -284,15 +314,17 @@
     return $(this).text();
    }).get();
 
+   console.log(data);
+
    $('#update_id').val(data[0]);
    $('#fname').val(data[1]);
    $('#lname').val(data[2]);
    $('#course').val(data[3]);
    $('#contact').val(data[4]);
-
   });
- })
+ });
  </script>
+
 
 </body>
 
