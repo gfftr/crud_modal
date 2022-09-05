8b51344d9689bf491b446d1f46b9ee6a77ca2cc3
gfftr <gff.terl@gmail.com>
2022-09-05 12:46:38 +0100

    Delete added

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
      ?>
 
 
-     <table id="datatableid" class="table table-bordered table-dark">
+     <table id="datatableid" class="table table-hover table-dark">
       <thead>
        <tr>
         <th scope="col"> ID</th>
@@ -205,9 +205,71 @@
 
 
 
+
+ <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
+ <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+   <div class="modal-content">
+    <div class="modal-header">
+     <h5 class="modal-title" id="exampleModalLabel"> Delete Student Data </h5>
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+     </button>
+    </div>
+
+    <form action="deletecode.php" method="POST">
+
+     <div class="modal-body">
+
+      <input type="hidden" name="delete_id" id="delete_id">
+
+      <h4> Do you want to Delete this Data ??</h4>
+     </div>
+     <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal"> NO </button>
+      <button type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button>
+     </div>
+    </form>
+
+   </div>
+  </div>
+ </div>
+
+
+
+
+
+
+
+
  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+
+
+ <script>
+ // delete
+ $().ready(function() {
+  $('.deletebtn').on('click', function() {
+   $('#deletemodal').modal('show');
+
+   $tr = $(this).closest('tr');
+
+   var data = $tr.children("td").map(function() {
+    return $(this).text();
+   }).get();
+
+   $('#delete_id').val(data[0]);
+
+  });
+ })
+ </script>
+
+
+
+
 
 
 
